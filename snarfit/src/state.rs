6f537444b0b9ccf5e3bf014a51715a0f51b3c923
Riko <xxy1836@gmail.com>
2025-03-20 17:58:32 +0800

    2025-3-20 works

--- snarfit/src/state.rs
+++ snarfit/src/state.rs
@@ -10,25 +10,17 @@ use crate::DEFAULT_CONFIG_FILE;
 use crate::DEFAULT_OUTPUT_DIRECTORY;
 use crate::FState;
 use crate::Mode;
+use crate::SKIP;
+use crate::SSpec;
+use crate::Type;
 
 #[derive(Error, Debug)]
 pub enum StateError {
     #[error("data store disconnected")]
     Disconnect(#[from] io::Error),
-    #[error("the data for key `{0}` is not available")]
-    Redaction(String),
-    #[error("invalid header (expected {expected:?}, found {found:?})")]
-    InvalidHeader { expected: String, found: String },
-    #[error("unknown data store error")]
-    Unknown,
 }
 
-fn set_config_file(path: &str) -> Result<PathBuf, StateError> {
-    let path = fs::canonicalize(path)?;
-    Ok(path)
-}
-
-fn set_output_directory(path: &str) -> Result<PathBuf, StateError> {
+pub fn set_canonicalize_file(path: &str) -> Result<PathBuf, StateError> {
     let path = fs::canonicalize(path)?;
     Ok(path)
 }
@@ -41,16 +33,50 @@ pub fn initialize_state() -> Result<FState, StateError> {
         audit_file_name: String::new(),
         audit_file: None,
         mode: Mode::None,
-        input_file: None,
+        input_file: String::new(),
         fileswritten: 0,
         block_size: 512,
-        config_file: set_config_file(DEFAULT_CONFIG_FILE)?,
-        output_directory: set_output_directory(DEFAULT_OUTPUT_DIRECTORY)?,
+        config_file: set_canonicalize_file(DEFAULT_CONFIG_FILE)?,
+        output_directory: set_canonicalize_file(DEFAULT_OUTPUT_DIRECTORY)?,
         invocation: args.join(" "),
         chunk_size: CHUNK_SIZE,
-        num_builtin: 0,
-        skip: 0,
-        time_stamp: Local::now(),
+        skip: SKIP,
     };
     Ok(s)
+}
+
+pub fn init_builtin(
+    s: &mut FState,
+    type_: Type,
+    suffix: &str,
+    header: &str,
+    footer: &str,
+    header_len: usize,
+    footer_len: usize,
+    max_len: usize,
+    case_sen: usize,
+) {
+    let new_s_spec = SSpec {
+        type_,
+        suffix: suffix.to_string(),
+        num_markers: 0,
+        header_len,
+        footer_len,
+        max_len,
+        found: 0,
+        header: header.to_string(),
+        footer: footer.to_string(),
+        case_sen,
+        comment: String::new(),
+    };
+}
+
+pub fn set_search_def(file_type: &str) {
+    let file_type_split: Vec<&str> = file_type.split(",").map(|x| x.trim()).collect();
+
+    for f in file_type_split {
+        if f == "jpg" || f == "jpeg" {
+            init_builtin(s, type_);
+        }
+    }
 }
